22aaf9139f10877ab4c6d573fcb83356f8610f43
Roha Pathan <RohaAbroPathan@gmail.com>
2023-12-13 22:52:30 +0500

    created listing model and configured index.html to show all listings

--- commerce/commerce/auctions/models.py
+++ commerce/commerce/auctions/models.py
@@ -1,6 +1,20 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+categories_choices = [
+    "Toys", "Fashion"
+]
 
 class User(AbstractUser):
     pass
+
+
+
+class Listing(models.Model):
+    title = models.TextField(max_length=64, blank=False)
+    description = models.TextField(max_length=2000, blank=False)
+    image_url = models.TextField(blank=True)
+    categories = models.Choices(categories_choices)
+    bid = ...
+    
+    ...
