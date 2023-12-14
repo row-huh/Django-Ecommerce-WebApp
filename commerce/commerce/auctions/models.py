f9046f07421d0c05a36642cf9f8200559ccff5c8
Roha Pathan <RohaAbroPathan@gmail.com>
2023-12-14 13:07:16 +0500

    .

--- commerce/commerce/auctions/models.py
+++ commerce/commerce/auctions/models.py
@@ -4,3 +4,13 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    title = models.TextField(max_length=64, blank=False)
+    description = models.TextField(max_length=2000, blank=False)
+    imageURL = models.TextField(max_length=64, blank=True)
+    categories = models.TextChoices("toys", "fashion")
+    # bid
+    
+    
